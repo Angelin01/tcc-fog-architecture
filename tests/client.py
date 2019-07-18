be95e9cba8f5e2b8daa8c03dc32c2a9d1cb6913e
Angelin01 <angeloborsoiross@hotmail.com>
2019-07-18 20:17:53 -0300

    Basic client structure

---
+++ tests/client.py
@@ -0,0 +1,26 @@
+import aiocoap as coap
+import asyncio
+import gzip
+from sys import argv, exit
+
+
+async def main(host: str, uri: str, method: coap.codes = coap.GET, payload: dict = None):
+	compressed_payload = payload
+
+	protocol = await coap.Context.create_client_context()
+
+	request = coap.Message(code=method, uri=f'coap://{host}/{uri}', payload=compressed_payload)
+
+	try:
+		response = await protocol.request(request).response
+	except Exception as e:
+		print('Failed to fetch resource:')
+		print(e)
+		exit(1)
+
+	print(f'Response code: {response.code}'
+	      f'Payload: {response.payload}')
+
+
+if __name__ == '__main__':
+	asyncio.get_event_loop().run_until_complete(main())
