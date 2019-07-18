3917f474fa43c9a87482c6d3870957ade5a36f3d
Angelin01 <angeloborsoiross@hotmail.com>
2019-07-18 20:55:11 -0300

    Completed main for client, just needs testing

--- tests/client.py
+++ tests/client.py
@@ -6,7 +6,7 @@ from sys import argv, exit
 
 
 async def main(host: str, uri: str, method: coap.codes = coap.GET, payload: dict = None):
-	compressed_payload = compress(bytes(json.dumps(payload, separators=(',', ':'), force_ascii=True), 'ascii'), 9)
+	compressed_payload = compress(bytes(json.dumps(payload, separators=(',', ':'), force_ascii=True), 'ascii'), 9) if payload else None
 
 	protocol = await coap.Context.create_client_context()
 
@@ -24,5 +24,46 @@ async def main(host: str, uri: str, method: coap.codes = coap.GET, payload: dict
 
 
 if __name__ == '__main__':
+	if not 2 < len(argv) < 6:
+		print(f'Usage: {argv[0]} host uri [method] [payload json str|file]')
+		exit(1)
 
-	asyncio.get_event_loop().run_until_complete(main())
+	HOST = argv[1]
+	if not isinstance(HOST, str):
+		print('Invalid host, must be a str')
+		exit(1)
+	HOST = HOST.rstrip('/')
+
+	URI = argv[2]
+	if not isinstance(URI, str):
+		print('Invalid uri, must be a str')
+		exit(1)
+	URI = URI.lstrip('/')
+
+	METHOD = None
+	if len(argv) >= 4:
+		if argv[3] == 'get' or argv[3] == 'GET':
+			METHOD = coap.GET
+		elif argv[3] == 'post' or argv[3] == 'POST':
+			METHOD = coap.POST
+		elif argv[3] == 'put' or argv[3] == 'PUT':
+			METHOD = coap.PUT
+		elif argv[3] == 'delete' or argv[3] == 'DELETE':
+			METHOD = coap.DELETE
+		else:
+			print(f'Invalid method {argv[3]}, use GET POST PUT or DELETE')
+			exit(1)
+
+	PAYLOAD = None
+	if len(argv) >= 5:
+		try:
+			PAYLOAD = json.loads(argv[4])
+		except ValueError:
+			try:
+				with open(argv[4]) as json_file:
+					PAYLOAD = json_file.read()
+			except FileNotFoundError:
+				print(f'{argv[4]} is not a valid JSON string or JSON file')
+				exit(1)
+
+	asyncio.run(main(HOST, URI, METHOD, PAYLOAD))
