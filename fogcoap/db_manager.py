a1bd9bc4e0b5c2c4db4afa0cf9897d8f5a7f899f
Angelin01 <angeloborsoiross@hotmail.com>
2019-07-21 17:42:58 -0300

    Create index and leave uniqueness checking to mongo

--- fogcoap/db_manager.py
+++ fogcoap/db_manager.py
@@ -1,6 +1,6 @@
 import pymongo
 import re
-from pymongo.errors import ConnectionFailure
+from pymongo.errors import ConnectionFailure, DuplicateKeyError
 from bson.objectid import ObjectId
 from datetime import datetime
 from typing import Union, Tuple, NewType
@@ -11,6 +11,7 @@ GDR = NewType('GenDatetimeRange', Union[Tuple[str, str], Tuple[int, int], Tuple[
 
 class DatabaseManager:
 	_ClientRegistry = 'client_registry'
+	_ClientNameIndex = 'name_index'
 	_TypeMetadata = 'type_metadata'
 	_Data = 'data'
 
@@ -22,22 +23,26 @@ class DatabaseManager:
 		except ConnectionFailure:
 			print(f'Database connection to {host}:{port} failed', flush=True)
 			raise
-
 		self._database = self._client[database]
+
 		self._client_registry = self._database[self._ClientRegistry]
+		if self._ClientNameIndex not in self._client_registry.index_information():
+			self._client_registry.create_index('name', name=self._ClientNameIndex, unique=True)
+
 		self._type_metadata = self._database[self._TypeMetadata]
 		self._data = self._database[self._Data]
 		self.warn_similarities = warn_similarities
 
-	def register_client(self, client: str) -> Union[bool, ObjectId]:
-		if self._client_registry.count_documents({'name': client}, limit=1) != 0:
-			if self.warn_similarities:
-				similar_names = self._client_registry.count_documents({'name': re.compile(client, re.IGNORECASE)})
-				if similar_names > 0:
-					print(f'Warning: there are {similar_names} clients with similar names to {client}')
-			return self._client_registry.insert_one({'name': client}).inserted_id
+	def register_client(self, client: str) -> ObjectId:
+		similar_names = 0
+		if self.warn_similarities:
+			similar_names = self._client_registry.count_documents({'name': re.compile(client, re.IGNORECASE)})
 
-		return False
+		obj_id = self._client_registry.insert_one({'name': client}).inserted_id
+		if similar_names > 0:
+			print(f'Warning: there are {similar_names} clients with similar names to {client}')
+		
+		return obj_id
 
 	def register_datatype(self, name: str, storage_type: Union[int, float, complex, str],
 	                      unit: str = None, valid_bounds: tuple = None, alert_thresholds: tuple = None) -> bool:
