18f6d646ca182d693ad26e5f6b0102e5dbdb68fc
Angelin01 <angeloborsoiross@hotmail.com>
2019-07-21 18:52:01 -0300

    Use proper logger instead of prints
    Will be useful in the future, I hope

--- fogcoap/db_manager.py
+++ fogcoap/db_manager.py
@@ -1,5 +1,6 @@
 import pymongo
 import re
+import logging
 from pymongo.errors import ConnectionFailure, DuplicateKeyError
 from bson.objectid import ObjectId
 from datetime import datetime
@@ -7,6 +8,7 @@ from typing import Union, Tuple, NewType
 
 
 GDR = NewType('GenDatetimeRange', Union[Tuple[str, str], Tuple[int, int], Tuple[datetime, datetime]])
+database_logger = logging.Logger(__name__)
 
 
 class DatabaseManager:
@@ -27,20 +29,43 @@ class DatabaseManager:
 		:param warn_similarities: When set to true, the Manager will throw warning when creating datatypes or registering clients with similar
 		                          names to ones previously created.
 		"""
+		# Setup logger #
+		# ======================= #
+		formatter = logging.Formatter(f'%(asctime)s - {database}@{host}:{port} - %(levelname)s - %(message)s')
+		handler = logging.StreamHandler()
+		handler.setFormatter(formatter)
+
+		if warn_similarities:
+			handler.setLevel(logging.WARNING)
+		else:
+			handler.setLevel(logging.ERROR)
+
+		database_logger.addHandler(handler)
+		# ======================= #
+
+		# Connect to database and setup data structure #
+		# ======================= #
+		
 		self._client = pymongo.MongoClient(host, port)
 		try:
 			# The ismaster command is cheap and does not require auth.
 			self._client.admin.command('ismaster')
 		except ConnectionFailure:
-			print(f'Database connection to {host}:{port} failed', flush=True)
+			database_logger.critical(f'Database connection to {host}:{port} failed', flush=True)
 			raise
+		database_logger.info(f'Connected to database {database} on {host}:{port}')
+
 		self._database = self._client[database]
 
 		self._client_registry = self._database[self._ClientRegistry]
 		self._client_registry.create_index('name', name=self._ClientNameIndex, unique=True)
+		database_logger.debug('Created index on client names')
 
 		self._type_metadata = self._database[self._TypeMetadata]
 		self._data = self._database[self._Data]
+		
+		# ======================= #
+		
 		self.warn_similarities = warn_similarities
 
 	def register_client(self, client: str) -> ObjectId:
@@ -49,13 +74,28 @@ class DatabaseManager:
 		:param client: The client's name.
 		:return: The ObjectId for the client in the database.
 		"""
+		# ======================= #
+		# Check for similarities if needed #
 		similar_names = 0
 		if self.warn_similarities:
 			similar_names = self._client_registry.count_documents({'name': re.compile(client, re.IGNORECASE)})
+		# ======================= #
 
-		obj_id = self._client_registry.insert_one({'name': client}).inserted_id
+		# ======================= #
+		# Actual insert
+		try:
+			obj_id = self._client_registry.insert_one({'name': client}).inserted_id
+		except DuplicateKeyError:
+			database_logger.error(f'Failed to add client {client} as it\'s a duplicate')
+			raise
+		database_logger.info(f'Registered new client {client} with id {obj_id}')
+		# ======================= #
+
+		# ======================= #
+		# Warn for similarities if necessary #
 		if similar_names > 0:
-			print(f'Warning: there are {similar_names} clients with similar names to {client}')
+			database_logger.warning(f'Warning: there are {similar_names} clients with similar names to {client}')
+		# ======================= #
 
 		return obj_id
 
@@ -72,3 +112,7 @@ class DatabaseManager:
 
 	def query_data_type(self, datatype: Union[str, ObjectId] = None, date_range: GDR = None) -> dict:
 		pass
+
+	@staticmethod
+	def set_logging_level(level: Union[logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL]) -> None:
+		database_logger.setLevel(level)
