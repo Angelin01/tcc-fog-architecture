cb40c208ec31b3f77bcfe00d98856b50b9f3551d
Angelin01 <angeloborsoiross@hotmail.com>
2019-08-24 14:55:30 -0300

    Basic instancing of clients

--- fogcoap/__main__.py
+++ fogcoap/__main__.py
@@ -4,6 +4,8 @@ import fogcoap
 def main():
 	db = fogcoap.DatabaseManager('fogcoap')
 	broker = fogcoap.Broker(db)
+	broker.run()
+	print('After broker run, shouldn\'t happen')
 
 
 if __name__ == '__main__':
