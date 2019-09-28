fde06e7582b204be8e0dc628dec82544427d68b6
Angelin01 <angeloborsoiross@hotmail.com>
2019-09-28 16:37:51 -0300

    Proper ClientAlert

--- fogcoap/alerts.py
+++ fogcoap/alerts.py
@@ -1,31 +1,18 @@
-import asyncio
-from datetime import datetime
-from aiocoap import Code, Message
+import json
+from aiocoap import Message
 from aiocoap.resource import ObservableResource
+from gzip import compress as gzcompress
 
 
 class ClientAlert(ObservableResource):
 	def __init__(self):
 		super().__init__()
 		
-		self._should_alert = False
-		self._loop = asyncio.get_event_loop()
-		self._handle = None
+		self._last_alert = b''
 		
-	def notify(self):
+	def notify(self, alert: dict):
+		self._last_alert = gzcompress(json.dumps(alert, separators=(',', ':'), ensure_ascii=True).encode('ascii'))
 		self.updated_state()
-		self._handle = self._loop.call_later(5, self.notify)
-	
-	def update_observation_count(self, new_count: int):
-		if new_count > 0 and self._handle is None:
-			print('Restarting clock')
-			self._handle = self._loop.call_later(5, self.notify)
-			
-		elif new_count == 0 and self._handle is not None:
-			print('Stopping clock')
-			self._handle.cancel()
-			self._handle = None
 	
 	async def render_get(self, request: Message):
-		payload = str(int(datetime.utcnow().timestamp())).encode('ascii')
-		return Message(payload=payload)
+		return Message(payload=self._last_alert)
