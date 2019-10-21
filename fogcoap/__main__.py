7ca3c20f8b0aae3dd00dddee96323f62a3a6ac3a
Angelin01 <angeloborsoiross@hotmail.com>
2019-10-21 18:43:02 -0300

    Make __main__ read arguments

--- fogcoap/__main__.py
+++ fogcoap/__main__.py
@@ -1,9 +1,18 @@
 import fogcoap
+from sys import argv
 
 
 def main():
+	database = 'fogcoap'
+	uri = 'mongodb://localhost'
+	if len(argv) >= 3:
+		database = argv[2]
+	
+	if len(argv) >= 4:
+		uri = argv[3]
+	
 	print('Connecting to database')
-	dm = fogcoap.DataManager('fogcoap')
+	dm = fogcoap.DataManager(database, uri)
 	print('Starting broker')
 	broker = fogcoap.Broker(dm)
 	broker.run()
